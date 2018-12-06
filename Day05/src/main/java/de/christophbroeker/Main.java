eb9d449be4283fdaf7750d63cf4de100dbba12c7
Christoph Bröker <development@christophbroeker.de>
2018-12-06 02:45:48 +0100

    Added implementation for second issue

--- Day05/src/main/java/de/christophbroeker/Main.java
+++ Day05/src/main/java/de/christophbroeker/Main.java
@@ -1,17 +1,46 @@
 package de.christophbroeker;
 
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
 public class Main {
 
     public static void main(String[] args) {
         String units = "";
-        String result = analyzePolymer(units);
-        System.out.println(result.length());
+        System.out.println(analyzePolymer(units).length());
+        System.out.println(reduceAndAnalyzePolymer(units));
+    }
+
+
+    /**
+     *
+     * @param polymer The initial polymer.
+     * @return The length of shortest polymer.
+     */
+    public static int reduceAndAnalyzePolymer(String polymer){
+        IntStream intStream = polymer.chars();
+        HashMap<String, Integer> map = new HashMap<>();
+        intStream.forEach(character -> {
+            String unit = String.valueOf(Character.toChars(character)).toLowerCase();
+            if(!map.containsKey(unit)){
+                map.put(unit, analyzePolymer(reducePolymer(polymer, unit)).length());
+            }
+        });
+
+        Optional<Integer> result = map.values().stream().min(Comparator.naturalOrder());
+        return result.orElse(0);
+    }
+
+    private static String reducePolymer(String polymer, String character){
+        return polymer.replaceAll(character, "").replaceAll(character.toUpperCase(), "");
     }
 
     /**
      *
      * @param startPolymer The startPolymer
-     * @return The shorted polymer.
+     * @return The length of the shorted polymer.
      */
     static String analyzePolymer(String startPolymer){
         Result result = new Result("", new StringBuilder());
@@ -69,7 +98,7 @@ public class Main {
      * @param isLast The second unit is the last of the start polymer.
      * @return A {@link Result} containing the current result polymer and the next first unit.
      */
-    public static Result add(StringBuilder resultPolymer, String firstUnit, String secondUnit, boolean isLast){
+    private static Result add(StringBuilder resultPolymer, String firstUnit, String secondUnit, boolean isLast){
         resultPolymer.append(firstUnit);
         if(isLast){
             resultPolymer.append(secondUnit);
